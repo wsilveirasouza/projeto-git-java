623c51d13a9b6a3300ddd8f60f916534dcded103
wsilveirasouza <wsilveirasouza@gmail.com>
2018-06-07 22:40:45 -0300

    Envio de projeto para o github com NETBEANS 8.2

---
+++ projeto-git-java/src/br/com/projeto/git/app.java
@@ -0,0 +1,14 @@
+
+package br.com.projeto.git;
+
+public class app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Configurando o GIT com o NETBEANS 8.2");
+        System.out.println("url:https://github.com/wsilveirasouza/projeto-git-java.git");
+    }
+    
+}
